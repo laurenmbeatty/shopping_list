35798902b9e15e13aee4931c27fecdc4f543c235
Lauren Beatty <lauren.m.beatty@gmail.com>
2015-10-04 07:08:01 -0500

    finished; added animation to li removal; finished styling

--- client/app.js
+++ client/app.js
@@ -6,10 +6,10 @@ $(document).ready(function () {
         $.ajax({
             url: "/note/" + name,
         });
-    };
+    }
 
     $(".toAppend").on('click', function () {
-        $("input").addClass("show-input").fadeIn(1000).focus();
+        $("input").addClass("show-input").fadeIn("slow").focus();
         $(".toAppend").fadeOut(1000);
     });
 
@@ -22,45 +22,51 @@ $(document).ready(function () {
                 "<span class='glyphicon glyphicon-ok'></span></button>" + $text.val() + "</li>");
             $(".userList").append($newListItem);
             $text.val('');
-            //clicking on checkbox removes list item and puts focus back in text box
-            $(".check-box").on('click', function () {
-                $("li:hover").fadeOut(500);
-                $("input").focus();
 
+            //clicking on checkbox removes list item with animation and puts focus back in text box
+            $('li').on('click', function() {
+                $(this).animate({
+                    opacity: 0.0,
+                    paddingLeft: '+=40'
+                }, 500, function() {
+                    $(this).remove();
+                });
+                $("input").focus();
             });
             return false;
         }
     });
 });
 
-
-
-        //$("li").on('click', function () {
-        //        var $newText = $(this).text();
-        //        var $newTextBox = $("<input type=text placeholder='" + $newText + "'class='newBox'" + "/>");
-        //        $(this).replaceWith($newTextBox);
-        //
-        //    //then after editing text box user can press enter and replace
-        //    $('input').keypress(function (e) {
-        //        if (e.which == 13) {
-        //            var $newText = $('input:text');
-        //            sendNoteToServer($newText.val());
-        //            var $brandNewListItem = $("<li class='sortableItem'>" + "<button class='btn-xs check-box'>" +
-        //                "<span class='glyphicon glyphicon-ok'></span></button>" + $newText.val() + "</li>");
-        //            $(".newBox").replaceWith($brandNewListItem);
-        //            //$(".newBox").remove();
-        //
-        //            $text.val('');
-        //        }
-        //
-        //    });
-        //});
-
-
-
-
-
-
+    //$(document).on('click', 'li', function () {
+    //    console.log('clicked');
+    //    var $newText = $(this).text();
+    //    var $newTextBox = $("<input type=text placeholder='" + $newText + "'class='newBox'" + "/>");
+    //    $(this).replaceWith($newTextBox);
+    //
+    //    //then after editing text box user can press enter and replace
+    //    $('input').keypress(function (e) {
+    //        if (e.which == 13) {
+    //            var $newText = $('input:text');
+    //            sendNoteToServer($newText.val());
+    //            var $brandNewListItem = $("<li class='sortableItem'>" + "<button class='btn-xs check-box'>" +
+    //                "<span class='glyphicon glyphicon-ok'></span></button>" + $newText.val() + "</li>");
+    //            $(this.newBox).replaceWith($brandNewListItem);
+    //            $(".newBox").remove();
+    //
+    //            $newText.val('');
+    //            $(".check-box").on('click', function () {
+    //                $(this.li).fadeOut(500);
+    //                $(".newBox").remove();
+    //                $(".addItem").focus();
+    //
+    //            });
+    //            return false;
+    //        }
+    //
+    //    });
+    //
+    //});
 
 
 
